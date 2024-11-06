040b75096d770e816a781c044f173c70193d4cec
Jenn Kaphammer <jeka7802@colorado.edu>
2024-11-06 16:19:18 -0700

    pygame window layout

--- game.py
+++ game.py
@@ -7,3 +7,29 @@ import matplotlib.pyplot as plt
 
 print("Success")
 
+pygame.init()
+screenWidth = 720
+screenHeight = 720
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+clock = pygame.time.Clock()
+running = True
+caption = pygame.display.set_caption('AI Racecar')
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("darkgreen")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
